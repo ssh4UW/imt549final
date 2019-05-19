01508dd28837213e2f7431d6ee8d46b167b72009
Stephen Holmes <stephenshigemi@gmail.com>
2019-05-18 23:47:41 -0700

    Built JS object

---
+++ js/clothes.js
@@ -0,0 +1,122 @@
+var clothes = [
+  {
+    itemid: 1,
+    title: "shoes",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "shoes", "formal", "leather"],
+    price: "175.00",
+    image: {
+      url: "pexels-photo-298863.jpeg",
+      text: "leather shoes"
+    }
+  },
+  {
+    itemid: 2,
+    title: "shirt",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "shirt", "formal", "cotton"],
+    price: "50.00",
+    image: {
+      url: "pexels-photo-297933.jpeg",
+      text: "folded shirt"
+    }
+  },
+  {
+    itemid: 3,
+    title: "t-shirt",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "tshirt", "casual", "cotton", "graphic"],
+    price: "20.00",
+    image: {
+      url: "pexels-photo-1099902.jpeg",
+      text: "graphic t-shirt"
+    }
+  },
+  {
+    itemid: 4,
+    title: "distressed jeans",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "jeans", "casual", "denim", "trendy"],
+    price: "50.00",
+    image: {
+      url: "pexels-photo-1129019.jpeg",
+      text: "disressed jeans"
+    }
+  },
+  {
+    itemid: 5,
+    title: "jacket",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "jacket", "fall", "warm"],
+    price: "100.00",
+    image: {
+      url: "pexels-photo-1518489.jpeg",
+      text: "man wearing jacket"
+    }
+  },
+  {
+    itemid: 6,
+    title: "t-shirt",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "tshirt", "casual", "cotton"],
+    price: "20.00",
+    image: {
+      url: "pexels-photo-1600009.jpeg",
+      text: "red graphic t-shirt"
+    }
+  },
+  {
+    itemid: 7,
+    title: "jacket",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "jacket", "casual"],
+    price: "70.00",
+    image: {
+      url: "pexels-photo-2061902.jpeg",
+      text: "man in jacket"
+    }
+  },
+  {
+    itemid: 8,
+    title: "denim jacket",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "denim", "casual", "jacket"],
+    price: "70.00",
+    image: {
+      url: "pexels-photo-2077976.jpeg",
+      text: "man in denim jacket"
+    }
+  },
+  {
+    itemid: 9,
+    title: "sweatshirt",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "plaid", "casual", "cotton", "sweatshirt"],
+    price: "40.00",
+    image: {
+      url: "pexels-photo-2079167.jpeg",
+      text: "man in sweatshirt with guitar"
+    }
+  },
+  {
+    itemid: 10,
+    title: "t-shirt",
+    description:
+      "Exercitation magna qui consectetur enim sit aliquip exercitation elit sit. Elit est voluptate in nostrud qui et mollit do et.",
+    tags: ["men", "tshirt", "casual", "cotton", "graphic"],
+    price: "20.00",
+    image: {
+      url: "pexels-photo-2213141.jpeg",
+      text: "graphic t-shirt"
+    }
+  }
+];
