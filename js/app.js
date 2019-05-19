89b7e5070700b7780100de0c4c97b0441ba703ef
Stephen Holmes <stephenshigemi@gmail.com>
2019-05-19 12:40:44 -0700

    Fix card layout

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ function renderCell(clothes) {
     cell.classList.add("mdl-cell");
     cell.classList.add("mdl-cell--12-col-tablet");
     cell.classList.add("mdl-cell--6-col-desktop");
+
     //cell.setAttribute("id", "card-container");
     window.appendChild(cell);
     renderCard(clothes[idx], cell);
@@ -25,29 +26,61 @@ function renderCell(clothes) {
 
 //render one card for a piece of clothing
 function renderCard(item, cell) {
-  //var cell = document.getElementById("card-container").lastChild;
+  //   var test = document.createElement("div");
+  //   test.classList.add("demo-card-square");
+  //   test.classList.add("mdl-card");
+  var outerCard = document.createElement("div");
+  outerCard.classList.add("demo-card-square");
+  outerCard.classList.add("mdl-card");
+  outerCard.classList.add("card");
+  //cell.appendChild(outerCard);
+  //create card div using mdl and css style
   var card = document.createElement("div");
   card.classList.add("mdl-card_title");
   card.classList.add("mdl-card--expand");
-  card.classList.add("card");
-  card.setAttribute("style", "background:url(images/" + item.image.url);
-  //card.setAttribute("style", "background-size: auto");
-
+  //   card.classList.add("card");
+  card.style.backgroundImage = "url(images/" + item.image.url;
+  card.style.backgroundSize = "cover";
+  card.style.backgroundPosition = "center";
+  //card title banner
   var h2 = document.createElement("h2");
-  h2.textContent = item.title;
+  h2.textContent = item.title.toUpperCase();
   h2.classList.add("mdl-card__title-text");
-  //   var img = document.createElement("img");
-  //   img.setAttribute("src", "images/" + item.image.url);
-  //   img.setAttribute("alt", item.image.text);
+  //item description
   var supportingText = document.createElement("div");
   supportingText.textContent = item.description;
-  supportingText.classList.add("mdl-card__supporting-text");
 
-  card
-    .appendChild(h2)
-    //.appendChild(img)
-    .insertAdjacentElement("afterend", supportingText);
-  cell.appendChild(card);
+  supportingText.classList.add("mdl-card__supporting-text");
+  supportingText.classList.add("description");
+  //item price
+  var price = document.createElement("div");
+  //price.classList.add("price");
+  price.classList.add("card-details");
+  price.classList.add("detail");
+  price.textContent = "Price: $" + item.price;
+  //item tags
+  var tags = document.createElement("p");
+  tags.classList.add("card-details");
+  tags.classList.add("tags");
+  tags.textContent = "tags: " + item.tags.join(", ");
+
+  card.appendChild(h2);
+  outerCard.appendChild(card);
+  outerCard.appendChild(supportingText);
+  outerCard.appendChild(price);
+  outerCard.appendChild(tags);
+  cell.appendChild(outerCard);
+  //   //.appendChild(img)
+  //   //card.insertAdjacentElement("afterend", supportingText);
+  //   //test.appendChild(card);
+  //   //card.appendChild(supportingText);
+  //   //card.appendChild(price);
+  //   //card.appendChild(tags);
+  //   cell.appendChild(card);
+  //   cell
+  //     .appendChild(supportingText)
+  //     .insertAdjacentElement("afterend", price)
+  //     .insertAdjacentElement("afterend", tags);
 }
 renderNestedGrid();
 renderCell(clothes);
